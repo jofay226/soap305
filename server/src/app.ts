96238e026101a792c974f068a50a7b80c0c9e329
Javokhir Fayzullaev <jofay91@gmail.com>
2026-02-22 14:39:05 +0500

    second commit

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import userRoute from './routes/user.route.ts'
 
 const app = express();
 dotenv.config();
 
+app.use(bodyParser.text({type: "text/xml"}))
+app.use('/api/soap', userRoute)
 
 
 
